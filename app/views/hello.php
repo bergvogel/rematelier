52e411aeb4e360d995de744025c8c7175610b145
bergvogel <hilbert.kruidenier@gmail.com>
2015-12-10 15:27:24 +0100

    update 2

--- app/views/hello.php
+++ app/views/hello.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title> rematelrier framework</title>
+	<title> rematelier framework</title>
 	<style>
 		@import url(//fonts.googleapis.com/css?family=Lato:700);
 
